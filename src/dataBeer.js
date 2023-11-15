c3f94496dfe2a707099d752a2e3ccc8e18cc4a45
Dayalt <128409558+Dayalt@users.noreply.github.com>
2023-11-15 19:41:49 -0300

    Agrega componente CardBeer, datosBeer.js y directorio de imágenes

---
+++ src/dataBeer.js
@@ -0,0 +1,61 @@
+const dataBeer = [
+    {
+        nombre: 'Andes IPA',
+        imagen: 'Ipa',
+        color: 'Dorado',
+        descripcion: 'Amargo y Cítrico',
+        abv: '6.5%', 
+    },
+    {
+        nombre: 'Amber Ale',
+        imagen: 'Amber',
+        color: 'Ambarino',
+        descripcion: 'Equilibrado, Caramelizado',
+        abv: '6.0%',
+    },
+    {
+        nombre: 'Cumbre Pale Ale',
+        imagen: 'Cumbre',
+        color: 'Ámbar',
+        descripcion: 'Frutal y Cítrico',
+        abv: '5,0%', 
+    },
+    {
+        nombre: 'Lagger',
+        imagen: 'Lagger',
+        color: 'Dorado',
+        descripcion: 'Refrescante y Suave',
+        abv: '4.5%', 
+    },
+    {
+        nombre: 'Anntares Stout',
+        imagen: 'Anntares',
+        color: 'Negro',
+        descripcion: 'Tostado y Chocolate',
+        abv: '6.2%', 
+    },
+    {
+        nombre: 'Andes Barleywine',
+        imagen: 'Andes',
+        color: 'Caramelo',
+        descripcion: 'Dulce e Intenso',
+        abv: '9.0%', 
+    },
+    {
+        nombre: 'Bockk (Bockr)',
+        imagen: 'Isenbeck',
+        color: 'Marrón',
+        descripcion: 'Malteado y Caramelizado',
+        abv: '6.5%', 
+    },
+    {
+        nombre: 'Red Ale',
+        imagen: 'Kunstmann',
+        color: 'Rojo Cobrizo',
+        descripcion: 'Equilibrado y Afrutado',
+        abv: '5.8%', 
+    },
+
+]
+
+export default dataBeer;
